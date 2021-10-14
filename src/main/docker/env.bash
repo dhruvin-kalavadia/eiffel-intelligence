2171b6cc9a3c269990dac951a4a53a96bbc47d4a
Dhruvin Kalavadia <dhruvin.kalavadia@ericsson.com>
2021-10-14 16:35:09 +0530

    Mongo version change in env bash file

--- src/main/docker/env.bash
+++ src/main/docker/env.bash
@@ -14,7 +14,7 @@ export KIBANA_PORT=8085
 export MONSTACHE_PORT=8086
 
 
-export MONGODB_IMAGE="mongo:4.4.9"
+export MONGODB_IMAGE="bitnami/mongo:latest"
 export RABBITMQ_IMAGE="bitnami/rabbitmq:3.8-debian-9"
 export EIFFEL_ER_IMAGE="eiffelericsson/eiffel-er:0.0.67"
 export JENKINS_IMAGE="bitnami/jenkins:2.138.3"
